9363a8f21596e34e5019c2e3614e234147ca854c
satluri7 <45723130+satluri7@users.noreply.github.com>
2019-10-20 16:49:21 -0700

    Create changedriveletter.ps1

---
+++ changedriveletter.ps1
@@ -0,0 +1,6 @@
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco install powershell --y
+choco install powershell.portable --y
+sleep 10s
+$PageFile = Get-WmiObject -Query "Select * From Win32_PageFileSetting Where Name='D:\\pagefile.sys'" -EnableAllPrivileges
+$PageFile.Delete()
